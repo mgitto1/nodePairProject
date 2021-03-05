bd4c04d67415df3395fdc04e0320a7d809274a11
Kevin Kim <kevin.kim9411@gmail.com>
2021-03-05 15:11:34 -0500

    modules

--- bash.js
+++ bash.js
@@ -1,10 +1,8 @@
-process.stdout.write("prompt >");
-process.stdin.on("data", (data) => {
-  const cmd = data.toString().trim();
-  if (cmd === "cwd") {
-    console.log(process.cwd());
-    process.stdout.write("\nprompt > ");
-  } else {
-    process.stdout.write("\nprompt > ");
-  }
-});
+const pwd = require('./pwd');
+
+pwd();
+
+
+const ls = require('./ls');
+
+ls();
---
+++ pwd.js
@@ -0,0 +1,14 @@
+
+
+module.exports = function () {
+process.stdout.write("prompt >");
+process.stdin.on("data", (data) => {
+  const cmd = data.toString().trim();
+  if (cmd === "pwd") {
+    console.log(process.cwd());
+    process.stdout.write("\nprompt > ");
+  } else {
+    process.stdout.write("\nprompt > ");
+  }
+});
+}
